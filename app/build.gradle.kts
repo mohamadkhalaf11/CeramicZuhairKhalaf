6c316f33c98f751fe74859837a31dd07b5ff92b1
mohamad <xxmohamad11@gmail.com>
2024-06-13 19:02:37 +0300

    app done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,15 +37,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.airbnb.android:lottie:3.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation ("com.google.android.gms:play-services-auth:21.1.1")
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-storage:21.0.0")
     implementation("com.google.firebase:firebase-firestore:25.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("androidx.navigation:navigation-ui:2.7.7")
@@ -53,6 +54,7 @@ dependencies {
     //implementation("com.google.android.gms:play-services-maps:18.0.1")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/ceramiczuhairkhalaf/Activities/AnimationActivity.java
@@ -0,0 +1,43 @@
+package com.example.ceramiczuhairkhalaf.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.ceramiczuhairkhalaf.Activities.MainActivity;
+import com.example.ceramiczuhairkhalaf.R;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    private LottieAnimationView lottieAnimationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_animation);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext() , MainActivity.class);
+                startActivity(i);
+            }
+        }, 5000);
+    }
+}
